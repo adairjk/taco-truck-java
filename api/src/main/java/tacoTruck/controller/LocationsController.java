b048664f1d9326f655cff95fb3efb594e929e6f6
Jarod Adair <jarod.adair@manheim.com>
2017-10-17 09:13:07 -0500

    add get by id to locations controller

--- api/src/main/java/tacoTruck/controller/LocationsController.java
+++ api/src/main/java/tacoTruck/controller/LocationsController.java
@@ -3,6 +3,7 @@ package tacoTruck.controller;
 import tacoTruck.model.Locations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -18,6 +19,11 @@ public class LocationsController {
     @GetMapping(path="")
     public @ResponseBody Iterable<Locations> getAllLocations() {
         return locationsRepository.findAll();
+    }
+
+    @GetMapping(path="/id/{id}")
+    public @ResponseBody Locations getById(@PathVariable("id") Long id) {
+        return locationsRepository.findOne(id);
     }
 
 }
